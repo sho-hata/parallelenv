c3396a83f83193a9dbcb98d3dc240c7cb5d1f948
sho-hata <syouki100241@gmail.com>
2022-05-20 00:29:20 +0900

    chore: skip if not a test file

--- parallelenv.go
+++ parallelenv.go
@@ -2,15 +2,15 @@ package parallelenv
 
 import (
 	"go/ast"
+	"strings"
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/ast/inspector"
 )
 
-const doc = "parallelenv is ..."
+const doc = "parallelenv is go linter that check whether environment variables are set in tests run in parallel"
 
-// Analyzer is ...
 var Analyzer = &analysis.Analyzer{
 	Name: "parallelenv",
 	Doc:  doc,
@@ -24,10 +24,16 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
 	nodeFilter := []ast.Node{
-		(*ast.Ident)(nil),
+		(*ast.File)(nil),
 	}
 
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
+		switch n := n.(type) {
+		case *ast.File:
+			if !strings.HasSuffix(pass.Fset.File(n.Pos()).Name(), "_test.go") {
+				return
+			}
+		}
 	})
 
 	return nil, nil
